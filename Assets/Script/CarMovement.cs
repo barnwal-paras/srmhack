6eb2e1761f80a754dfdab459c5c007fc78515de5
barnwal-paras <parasbarnwal06@gmail.com>
2017-10-06 14:40:18 +0530

    Initial libraries, sdks and 3dModels

---
+++ Assets/Script/CarMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+	public AvatarController avatar;
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/triggerPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerPoint : MonoBehaviour {
+
+	public AvatarController avatarpos;
+	public Vector3[] jointpos;
+
+	void Start () {
+		avatarpos = new AvatarController ();
+
+	}
+	
+
+	void Update () {
+		
+	}
+}
